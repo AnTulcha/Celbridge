3e44d4d9e34af43bc91229b397f4d389d1fe2310
Chris Gregan <chrisgregan@gmail.com>
2023-11-13 13:49:42 +0000

    Open a HTML document using the WebView2 control
    Set the StartURL property to open another URL when the document opens instead of loading the file contents.

---
+++ Celbridge/Celbridge/Models/HTMLResource.cs
@@ -0,0 +1,35 @@
+﻿namespace Celbridge.Models
+{
+    [ResourceType("HTML File", "A HTML file resource", "Page2", ".html")]
+    public class HTMLResource : FileResource, IDocumentEntity
+    {
+        public string StartURL { get; set; } = string.Empty;
+        
+        public static Result CreateResource(string path)
+        {
+            try
+            {
+                string htmlContent = @"
+<!DOCTYPE html>
+<html lang=""en"">
+<head>
+    <meta charset=""UTF-8"">
+    <meta name=""viewport"" content=""width=device-width, initial-scale=1.0"">
+    <title>Empty HTML Page</title>
+</head>
+<body>
+Empty HTML Page
+</body>
+</html>
+";
+                File.WriteAllText(path, htmlContent);
+            }
+            catch (Exception ex)
+            {
+                return new ErrorResult($"Failed to create file at '{path}'. {ex.Message}");
+            }
+
+            return new SuccessResult();
+        }
+    }
+}
