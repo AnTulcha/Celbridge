34fc0a3408146a43fb8e316ed1ebcd7268e33784
Chris Gregan <chrisgregan@gmail.com>
2024-02-01 14:50:22 +0000

    Add a unit test for console service

---
+++ Celbridge/Celbridge.Tests/ConsoleTests.cs
@@ -0,0 +1,50 @@
+﻿using Celbridge.BaseLibrary.Console;
+using Celbridge.BaseLibrary.Logging;
+using Celbridge.CommonServices.Logging;
+using Celbridge.CoreExtensions.Console;
+using CommunityToolkit.Mvvm.Messaging;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Celbridge.Tests;
+
+[TestFixture]
+public class ConsoleTests
+{
+    private ServiceProvider? _serviceProvider;
+
+    [SetUp]
+    public void Setup()
+    {
+        var services = new ServiceCollection();
+
+        // Mimic your application's service registration
+        // For example, register a real service
+        services.AddSingleton<IMessenger>(WeakReferenceMessenger.Default);
+        services.AddSingleton<IConsoleService, ConsoleService>();
+        services.AddSingleton<ILoggingService, LoggingService>();
+
+        _serviceProvider = services.BuildServiceProvider();
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        if (_serviceProvider != null)
+        {
+            (_serviceProvider as IDisposable)?.Dispose();
+        }
+    }
+
+    [Test]
+    public void TestPrintCommand()
+    {
+        // Resolve the service under test from the DI container
+        var loggingService = _serviceProvider!.GetRequiredService<IConsoleService>();
+
+        // Assert something about the service
+        bool result = loggingService.Execute("print");
+
+        // Todo: Use FluentResult to return a more informative success or error
+        result.Should().BeTrue();
+    }
+}
--- Celbridge/Celbridge.Tests/GlobalUsings.cs
+++ Celbridge/Celbridge.Tests/GlobalUsings.cs
@@ -1,3 +1,2 @@
-﻿global using Celbridge.Legacy.Models;
-global using FluentAssertions;
+﻿global using FluentAssertions;
 global using NUnit.Framework;
