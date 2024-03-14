9ca911337f544c525666aa4462c67faba85fdc16
Chris Gregan <chrisgregan@gmail.com>
2024-03-14 17:33:59 +0000

    Add console title bar with clear button
    Add strings for title and clear button tooltip text.

--- Celbridge/CoreExtensions/Celbridge.Console/Views/ConsolePanel.cs
+++ Celbridge/CoreExtensions/Celbridge.Console/Views/ConsolePanel.cs
@@ -1,4 +1,5 @@
 ﻿using Celbridge.Console.ViewModels;
+using Microsoft.Extensions.Localization;
 
 namespace Celbridge.Console.Views;
 
@@ -7,16 +8,22 @@ public sealed partial class ConsolePanel : UserControl
     private readonly FontFamily IconFontFamily = new FontFamily("Segoe MDL2 Assets");
     private const string StrokeEraseGlyph = "\ued60";
 
+    public string Title => _stringLocalizer.GetString($"{nameof(ConsolePanel)}.{nameof(Title)}");
+    public string ClearButtonTooltip => _stringLocalizer.GetString($"{nameof(ConsolePanel)}.{nameof(ClearButtonTooltip)}");
+
+    private IStringLocalizer _stringLocalizer;
+
     public ConsolePanelViewModel ViewModel { get; }
 
     public ConsolePanel()
     {
         var serviceProvider = ServiceLocator.ServiceProvider;
+        _stringLocalizer = serviceProvider.GetRequiredService<IStringLocalizer>();
+
         ViewModel = serviceProvider.GetRequiredService<ConsolePanelViewModel>();
 
         var clearButton = new Button()
-            .HorizontalAlignment(HorizontalAlignment.Left)
-            .VerticalAlignment(VerticalAlignment.Top)
+            .Grid(column: 2)
             .Command(ViewModel.ClearCommand)
             .Content(new FontIcon
             {
@@ -24,6 +31,29 @@ public sealed partial class ConsolePanel : UserControl
                 Glyph = StrokeEraseGlyph,
             });
 
-        Content = clearButton;
+        ToolTipService.SetToolTip(clearButton, ClearButtonTooltip);
+        ToolTipService.SetPlacement(clearButton, PlacementMode.Top);
+
+        var titleBar = new Grid()
+            .Background(ThemeResource.Get<Brush>("PanelBackgroundBrush"))
+            .BorderBrush(ThemeResource.Get<Brush>("PanelBorderBrush"))
+            .BorderThickness(0, 1, 0, 1)
+            .ColumnDefinitions("Auto, *, Auto, 48")
+            .Children(
+                new TextBlock()
+                    .Grid(column: 0)
+                    .Text(Title)
+                    .VerticalAlignment(VerticalAlignment.Center),
+                clearButton);
+
+        var panelGrid = new Grid()
+            .RowDefinitions("40, *, 32")
+            .Children(titleBar);
+           
+        //
+        // Set the data context and page content
+        // 
+        this.DataContext(ViewModel, (userControl, vm) => userControl
+            .Content(panelGrid));
     }
 }
