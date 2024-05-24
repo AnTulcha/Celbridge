6345a1b69b24c1a948f8df8eac261ff0f1ac1c97
Chris Gregan <chrisgregan@gmail.com>
2024-05-24 08:07:15 +0100

    Add ShowAlert() script util

--- Celbridge/CoreExtensions/Celbridge.Console/ViewModels/ConsolePanelViewModel.cs
+++ Celbridge/CoreExtensions/Celbridge.Console/ViewModels/ConsolePanelViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Celbridge.BaseLibrary.Console;
+using Celbridge.BaseLibrary.Tasks;
 using Celbridge.BaseLibrary.UserInterface;
 using CommunityToolkit.Mvvm.ComponentModel;
 
@@ -7,6 +8,7 @@ namespace Celbridge.Console.ViewModels;
 public partial class ConsolePanelViewModel : ObservableObject
 {
     private readonly IConsoleService _consoleService;
+    private readonly IUserInterfaceService _userInterfaceService;
 
     public event Action? OnClearConsole;
     public event Action? OnAddConsoleTab;
@@ -16,15 +18,28 @@ public partial class ConsolePanelViewModel : ObservableObject
         IConsoleService consoleService)
     {
         _consoleService = consoleService;
+        _userInterfaceService = userInterfaceService;
 
         // Register the singleton console service with the workspace service
         userInterfaceService.WorkspaceService.RegisterService(_consoleService);
     }
 
-    public ICommand ClearCommand => new RelayCommand(ClearCommand_Executed);
-    private void ClearCommand_Executed()
+    public ICommand ClearCommand => new AsyncRelayCommand(ClearCommand_Executed);
+    private async Task ClearCommand_Executed()
     {
-        OnClearConsole?.Invoke();
+        var schedulerService = ServiceLocator.ServiceProvider.GetRequiredService<ISchedulerService>();
+
+        schedulerService.ScheduleFunction(async () =>
+        {
+            var userInterfaceService = ServiceLocator.ServiceProvider.GetRequiredService<IUserInterfaceService>();
+            var dialogService = userInterfaceService.DialogService;
+
+            await dialogService.ShowAlertDialogAsync("1", "2", "Ok");
+            //await dialogService.ShowAlertDialogAsync(title, message, "Ok");
+        });
+
+        //var dialogService = _userInterfaceService.DialogService;
+        //await dialogService.ShowAlertDialogAsync("Some title", "Some message", "Ok");
     }
 
     public ICommand AddConsoleTabCommand => new RelayCommand(AddConsoleTabCommand_Executed);
--- Celbridge/CoreExtensions/Celbridge.ScriptUtils/CommonUtils.cs
+++ Celbridge/CoreExtensions/Celbridge.ScriptUtils/CommonUtils.cs
@@ -1,10 +1,33 @@
-﻿namespace Celbridge.ScriptUtils;
+﻿using Celbridge.BaseLibrary.Tasks;
+using Celbridge.BaseLibrary.UserInterface;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Celbridge.ScriptUtils;
 
 public class CommonUtils
 {
-    public static void DoTest(string text)
+    public class ShowAlert() : ITask
     {
-        Console.WriteLine(text);
+        public async Task<Result> ExecuteAsync(CancellationToken cancellationToken)
+        {
+            var userInterfaceService = ServiceLocator.ServiceProvider.GetRequiredService<IUserInterfaceService>();
+            var dialogService = userInterfaceService.DialogService;
+
+            await dialogService.ShowAlertDialogAsync("1", "2", "Ok");
+
+            return Result.Ok();
+        }
+    }
+
+    public static void Alert(string title, string message)
+    {
+        // Is this somehow not getting called on the main thread?
+        // Try somthing simple, like writing to the console service
+        // Isn't there somehting weird about show dialog - has to be called from the main thread or something
+        // Hang on - doesn't the scheduler start once there's a task - maybe it's using the wrong thread?
+
+        var schedulerService = ServiceLocator.ServiceProvider.GetRequiredService<ISchedulerService>();
+        schedulerService.ScheduleTask(new ShowAlert());
     }
 }
 
