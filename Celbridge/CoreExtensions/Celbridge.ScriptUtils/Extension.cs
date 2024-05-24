0893d962102df6ebd02cebf7145039387f13424c
Chris Gregan <chrisgregan@gmail.com>
2024-05-24 10:54:42 +0100

    Removed unneeded comments

--- Celbridge/CoreExtensions/Celbridge.ScriptUtils/Extension.cs
+++ Celbridge/CoreExtensions/Celbridge.ScriptUtils/Extension.cs
@@ -7,14 +7,10 @@ namespace Celbridge.ScriptUtils;
 public class Extension : IExtension
 {
     public void ConfigureServices(IExtensionServiceCollection config)
-    {
-    }
+    {}
 
     public Result Initialize()
     {
-        // Todo: Register the Util Classes here with the scripting service
-        // Todo: Scripting service loads the registered util classes and adds static usings for them.
-
         var scriptingService = ServiceLocator.ServiceProvider.GetRequiredService<IScriptingService>();
 
         scriptingService.AddContextSetupCommand("#r \"Celbridge.ScriptUtils\"");
