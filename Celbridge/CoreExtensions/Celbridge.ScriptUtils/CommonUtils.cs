62dfe2bfc08694c7d896e25bfdb54467fe98b0b3
Chris Gregan <chrisgregan@gmail.com>
2024-05-24 10:20:00 +0100

    Add Alert method to display an alert dialog from scripting

--- Celbridge/CoreExtensions/Celbridge.ScriptUtils/CommonUtils.cs
+++ Celbridge/CoreExtensions/Celbridge.ScriptUtils/CommonUtils.cs
@@ -1,33 +1,21 @@
-﻿using Celbridge.BaseLibrary.Tasks;
-using Celbridge.BaseLibrary.UserInterface;
+﻿using Celbridge.BaseLibrary.UserInterface;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Celbridge.ScriptUtils;
 
 public class CommonUtils
 {
-    public class ShowAlert() : ITask
+    public static void Alert(string title, string message)
     {
-        public async Task<Result> ExecuteAsync(CancellationToken cancellationToken)
+        // UI code must be run on the UI thread
+        var dispatcher = ServiceLocator.ServiceProvider.GetRequiredService<IDispatcher>();
+        dispatcher.ExecuteAsync(() =>
         {
             var userInterfaceService = ServiceLocator.ServiceProvider.GetRequiredService<IUserInterfaceService>();
             var dialogService = userInterfaceService.DialogService;
 
-            await dialogService.ShowAlertDialogAsync("1", "2", "Ok");
-
-            return Result.Ok();
-        }
-    }
-
-    public static void Alert(string title, string message)
-    {
-        // Is this somehow not getting called on the main thread?
-        // Try somthing simple, like writing to the console service
-        // Isn't there somehting weird about show dialog - has to be called from the main thread or something
-        // Hang on - doesn't the scheduler start once there's a task - maybe it's using the wrong thread?
-
-        var schedulerService = ServiceLocator.ServiceProvider.GetRequiredService<ISchedulerService>();
-        schedulerService.ScheduleTask(new ShowAlert());
+            dialogService.ShowAlertDialogAsync(title, message, "Ok");
+        });
     }
 }
 
