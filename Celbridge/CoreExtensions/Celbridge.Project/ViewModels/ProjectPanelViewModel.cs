25ffd5cd3fad0b59c57cee28d2fb4654c1298740
Chris Gregan <chrisgregan@gmail.com>
2024-06-09 22:14:16 +0100

    Simplify initialization of ProjectPanelViewModel

--- Celbridge/CoreExtensions/Celbridge.Project/ViewModels/ProjectPanelViewModel.cs
+++ Celbridge/CoreExtensions/Celbridge.Project/ViewModels/ProjectPanelViewModel.cs
@@ -1,34 +1,26 @@
 ﻿using Celbridge.BaseLibrary.Project;
 using Celbridge.BaseLibrary.UserInterface;
-using Celbridge.BaseLibrary.Workspace;
 using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace Celbridge.Project.ViewModels;
 
 public partial class ProjectPanelViewModel : ObservableObject
 {
-    private readonly IMessengerService _messengerService;
     private readonly IUserInterfaceService _userInterfaceService;
 
     [ObservableProperty]
     private string _titleText = string.Empty;
 
     public ProjectPanelViewModel(
-        IMessengerService messengerService,
         IUserInterfaceService userInterfaceService)
     {
-        _messengerService = messengerService;
         _userInterfaceService = userInterfaceService;
 
-        _messengerService.Register<WorkspaceInitializedMessage>(this, OnWorkspaceInitialized);
-    }
-
-    private void OnWorkspaceInitialized(object recipient, WorkspaceInitializedMessage message)
-    {
-        // Todo: Can this be acquired in the constructor instead?
-        IProjectService projectService = _userInterfaceService.WorkspaceService.ProjectService;
-
+        // The project data is guaranteed to have been loaded at this point, so it's safe to just
+        // acquire a reference via the ProjectService.
+        var projectService = _userInterfaceService.WorkspaceService.ProjectService;
         var projectData = projectService.LoadedProjectData;
+
         TitleText = projectData.ProjectName;
     }
 }
