912399cafc1ad59897e2cee7a47b7541dd90df0a
Chris Gregan <chrisgregan@gmail.com>
2024-06-10 11:12:44 +0100

    Remove unused dependencies in WorkspaceService

--- Celbridge/CoreExtensions/Celbridge.Workspace/WorkspaceService.cs
+++ Celbridge/CoreExtensions/Celbridge.Workspace/WorkspaceService.cs
@@ -1,7 +1,6 @@
 ﻿using Celbridge.BaseLibrary.Console;
 using Celbridge.BaseLibrary.Documents;
 using Celbridge.BaseLibrary.Inspector;
-using Celbridge.BaseLibrary.Messaging;
 using Celbridge.BaseLibrary.Project;
 using Celbridge.BaseLibrary.Status;
 using Celbridge.BaseLibrary.Workspace;
@@ -15,26 +14,19 @@ public class WorkspaceService : IWorkspaceService
     public bool IsRightPanelVisible { get; }
     public bool IsBottomPanelVisible { get; }
 
-    private readonly IServiceProvider _serviceProvider;
-    private readonly IMessengerService _messengerService;
-
     public IProjectService ProjectService { get; }
     public IStatusService StatusService { get; }
     public IConsoleService ConsoleService { get; }
     public IInspectorService InspectorService { get; }
     public IDocumentsService DocumentsService { get; }
 
-    public WorkspaceService(IServiceProvider serviceProvider,
-        IMessengerService messengerService)
+    public WorkspaceService(IServiceProvider serviceProvider)
     {
-        _serviceProvider = serviceProvider;
-        _messengerService = messengerService;
-
         // Create instances of the required sub-services
-        ProjectService = _serviceProvider.GetRequiredService<IProjectService>();
-        StatusService = _serviceProvider.GetRequiredService<IStatusService>();
-        ConsoleService = _serviceProvider.GetRequiredService<IConsoleService>();
-        InspectorService = _serviceProvider.GetRequiredService<IInspectorService>();
-        DocumentsService = _serviceProvider.GetRequiredService<IDocumentsService>();
+        ProjectService = serviceProvider.GetRequiredService<IProjectService>();
+        StatusService = serviceProvider.GetRequiredService<IStatusService>();
+        ConsoleService = serviceProvider.GetRequiredService<IConsoleService>();
+        InspectorService = serviceProvider.GetRequiredService<IInspectorService>();
+        DocumentsService = serviceProvider.GetRequiredService<IDocumentsService>();
     }
 }
