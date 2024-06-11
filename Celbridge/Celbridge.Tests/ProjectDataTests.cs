049e011d00207d1d39537e46ff16663fe0bf9e45
Chris Gregan <chrisgregan@gmail.com>
2024-06-11 15:49:19 +0100

    Create test folder in test Setup

--- Celbridge/Celbridge.Tests/ProjectDataTests.cs
+++ Celbridge/Celbridge.Tests/ProjectDataTests.cs
@@ -5,28 +5,40 @@ using CommunityToolkit.Diagnostics;
 namespace Celbridge.Tests;
 
 [TestFixture]
-public class ProjectTests
+public class ProjectDataTests
 {
     private IProjectDataService? _projectDataService;
+    private string? _projectFolder;
 
     [SetUp]
     public void Setup()
     {
+        _projectFolder = System.IO.Path.Combine(System.IO.Path.GetTempPath(), $"Celbridge/{nameof(ProjectDataTests)}");
+        if (Directory.Exists(_projectFolder))
+        {
+            Directory.Delete(_projectFolder, true);
+        }
+
         _projectDataService = new ProjectDataService();
     }
 
     [TearDown]
     public void TearDown()
-    {}
+    {
+        _projectDataService = null;
+        if (Directory.Exists(_projectFolder))
+        {
+            Directory.Delete(_projectFolder!, true);
+        }
+    }
 
     [Test]
     public async Task ICanCreateAndLoadProjectDataAsync()
     {
         Guard.IsNotNull(_projectDataService);
+        Guard.IsNotNullOrEmpty(_projectFolder);
 
-        var projectFolder = System.IO.Path.Combine(System.IO.Path.GetTempPath(), "TestProject");
-
-        var createResult = await _projectDataService.CreateProjectDataAsync(projectFolder, "TestA");
+        var createResult = await _projectDataService.CreateProjectDataAsync(_projectFolder, "TestProjectA");
         createResult.IsSuccess.Should().BeTrue();
 
         var projectPath = createResult.Value;
@@ -36,12 +48,12 @@ public class ProjectTests
 
         var projectData = _projectDataService.LoadedProjectData!;
         projectData.Should().NotBeNull();
-        projectData.ProjectName.Should().Be("TestA");
+        projectData.ProjectName.Should().Be("TestProjectA");
 
         _projectDataService.UnloadProjectData();
         _projectDataService.LoadedProjectData.Should().BeNull();
 
-        Directory.Delete(projectFolder, true);
+        Directory.Delete(_projectFolder, true);
         File.Exists(projectPath).Should().BeFalse();
     }
 }
