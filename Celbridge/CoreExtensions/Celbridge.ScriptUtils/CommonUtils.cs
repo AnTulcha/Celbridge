42b569a418484207a73d68e2bb9b0ab02f2ad1ad
Chris Gregan <chrisgregan@gmail.com>
2024-06-15 12:01:33 +0100

    Bind project commands to scripting

--- Celbridge/CoreExtensions/Celbridge.ScriptUtils/CommonUtils.cs
+++ Celbridge/CoreExtensions/Celbridge.ScriptUtils/CommonUtils.cs
@@ -1,5 +1,7 @@
-﻿using Celbridge.BaseLibrary.Dialog;
-using Celbridge.BaseLibrary.UserInterface;
+﻿using Celbridge.BaseLibrary.Commands;
+using Celbridge.BaseLibrary.Commands.Project;
+using Celbridge.BaseLibrary.Dialog;
+using Celbridge.BaseLibrary.Project;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Celbridge.ScriptUtils;
@@ -14,6 +16,31 @@ public class CommonUtils
         {
             var dialogService = ServiceLocator.ServiceProvider.GetRequiredService<IDialogService>();
             dialogService.ShowAlertDialogAsync(title, message, "Ok");
+        });
+    }
+
+    //
+    // Todo: Define these methods in Celbridge.Commands and bind them automically for scripting
+    //
+
+    public static void LoadProject(string projectPath)
+    {
+        var commandService = ServiceLocator.ServiceProvider.GetRequiredService<ICommandService>();
+        commandService.Execute<ILoadProjectCommand>(command => command.ProjectPath = projectPath);
+    }
+
+    public static void UnloadProject()
+    {
+        var commandService = ServiceLocator.ServiceProvider.GetRequiredService<ICommandService>();
+        commandService.Execute<IUnloadProjectCommand>();
+    }
+
+    public static void CreateProject(string projectName, string folder)
+    {
+        var commandService = ServiceLocator.ServiceProvider.GetRequiredService<ICommandService>();
+        commandService.Execute<ICreateProjectCommand>(command =>
+        {
+            command.Config = new NewProjectConfig(projectName, folder); 
         });
     }
 }
