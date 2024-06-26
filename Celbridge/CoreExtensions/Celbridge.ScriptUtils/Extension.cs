317c02c14e1670c7e3e93f1b413e30cb423a80b0
Chris Gregan <chrisgregan@gmail.com>
2024-06-26 23:36:35 +0100

    Bind static command methods in the constructor of Scripting Service

--- Celbridge/CoreExtensions/Celbridge.ScriptUtils/Extension.cs
+++ Celbridge/CoreExtensions/Celbridge.ScriptUtils/Extension.cs
@@ -1,8 +1,6 @@
-﻿using Celbridge.BaseLibrary.Commands;
-using Celbridge.BaseLibrary.Extensions;
+﻿using Celbridge.BaseLibrary.Extensions;
 using Celbridge.BaseLibrary.Scripting;
 using Microsoft.Extensions.DependencyInjection;
-using System.Reflection;
 
 namespace Celbridge.ScriptUtils;
 
@@ -20,70 +18,6 @@ public class Extension : IExtension
         scriptingService.AddContextSetupCommand("#r \"Celbridge.ScriptUtils\"");
         scriptingService.AddContextSetupCommand("using static Celbridge.ScriptUtils.Services.CommonUtils;");
 
-        //
-        // Bind to static methods defined on any class that inherits from CommandBase
-        //
-        try
-        {
-            BindStaticCommandMethods();
-        }
-        catch (Exception ex)
-        {
-            return Result.Fail($"Failed to initialize script support. {ex.Message}");
-        }
-
         return Result.Ok();
     }
-
-    private void BindStaticCommandMethods()
-    {
-        var scriptingService = ServiceLocator.ServiceProvider.GetRequiredService<IScriptingService>();
-
-        //
-        // Find all classes that inherit from CommandBase
-        //
-        var commandTypes = new List<Type>();
-        foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
-        {
-            var types = assembly.GetTypes()
-                                .Where(t => typeof(IExecutableCommand).IsAssignableFrom(t) && t.IsClass && !t.IsAbstract)
-                                .ToList();
-            commandTypes.AddRange(types);
-        }
-        commandTypes.Sort((a, b) => a.Name.CompareTo(b.Name));
-
-        //
-        // Find all assemblies that contain classes that inherit from CommandBase
-        //
-        var commandAssemblies = new List<Assembly>();
-        foreach (var type in commandTypes)
-        {
-            if (type != null)
-            {
-                commandAssemblies.AddDistinct(type.Assembly);
-            }
-        }
-        commandAssemblies.Sort((a, b) => a.GetName().Name!.CompareTo(b.GetName().Name!));
-
-        //
-        // Emit script to reference each assembly
-        //
-        foreach (var assembly in commandAssemblies)
-        {
-            var assemblyName = assembly.GetName().Name;
-            var script = $"#r \"{assemblyName}\"";
-            scriptingService.AddContextSetupCommand(script);
-        }
-
-        //
-        // Emit script to bind the static methods in each command class
-        //
-        foreach (var commandType in commandTypes)
-        {
-            var typeName = commandType.FullName;
-            var script = $"using static {typeName};";
-            scriptingService.AddContextSetupCommand(script);
-        }
-    }
-
 }
