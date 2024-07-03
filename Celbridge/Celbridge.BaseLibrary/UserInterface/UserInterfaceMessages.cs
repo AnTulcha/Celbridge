9448c3209dc72e1a7885361309e75f1ec5668681
Chris Gregan <chrisgregan@gmail.com>
2024-07-03 08:04:20 +0100

    Remove unused message

--- Celbridge/Celbridge.BaseLibrary/UserInterface/UserInterfaceMessages.cs
+++ Celbridge/Celbridge.BaseLibrary/UserInterface/UserInterfaceMessages.cs
@@ -9,8 +9,3 @@ public record MainWindowActivatedMessage();
 /// Sent when the main window has been deactivated (i.e. lost focus).
 /// </summary>
 public record MainWindowDeactivatedMessage();
-
-/// <summary>
-/// Sent when the user performs a keyboard shortcut action.
-/// </summary>
-public record ShortcutActionMessage(ShortcutAction shortcutAction);
