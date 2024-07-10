a496555d63e448027ba88492cdb2b92898894890
Chris Gregan <chrisgregan@gmail.com>
2024-07-10 08:24:17 +0100

    Initial ResourceNameValidator implementation

---
+++ Celbridge/Celbridge.BaseLibrary/Validators/IResourceNameValidator.cs
@@ -0,0 +1,15 @@
+﻿using Celbridge.BaseLibrary.Resources;
+
+namespace Celbridge.BaseLibrary.Validators;
+
+/// <summary>
+/// Applies a set of validation rules to an input string.
+/// </summary>
+public interface IResourceNameValidator : IValidator
+{
+    /// <summary>
+    /// Parent folder resource that will contain the proposed resource.
+    /// Used to check for conflicts with existing resource names.
+    /// </summary>
+    IFolderResource? ParentFolder { get; set; }
+}
---
+++ Celbridge/Celbridge.BaseLibrary/Validators/IValidator.cs
@@ -0,0 +1,13 @@
+﻿namespace Celbridge.BaseLibrary.Validators;
+
+/// <summary>
+/// Applies a set of validation rules to an input string.
+/// </summary>
+public interface IValidator
+{
+    /// <summary>
+    /// Returns true if the input string is valid.
+    /// The errors list contains the validation errors if the string is not valid.
+    /// </summary>
+    ValidationResult Validate(string input);
+}
