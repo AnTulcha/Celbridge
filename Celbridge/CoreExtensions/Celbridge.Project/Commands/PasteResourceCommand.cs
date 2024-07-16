d4a2c8cded497f91c905b0117b432f585e521170
Chris Gregan <chrisgregan@gmail.com>
2024-07-16 11:25:00 +0100

    Undo support for paste command

--- Celbridge/CoreExtensions/Celbridge.Project/Commands/PasteResourceCommand.cs
+++ Celbridge/CoreExtensions/Celbridge.Project/Commands/PasteResourceCommand.cs
@@ -1,7 +1,9 @@
 ﻿using Celbridge.BaseLibrary.Commands;
+using Celbridge.BaseLibrary.Dialog;
 using Celbridge.BaseLibrary.Project;
 using Celbridge.BaseLibrary.Resources;
 using Celbridge.BaseLibrary.Workspace;
+using Microsoft.Extensions.Localization;
 using Windows.ApplicationModel.DataTransfer;
 using Windows.Storage;
 
@@ -11,20 +13,56 @@ public class PasteResourceCommand : CommandBase, IPasteResourceCommand
 {
     private readonly IMessengerService _messengerService;
     private readonly IWorkspaceWrapper _workspaceWrapper;
+    private readonly IStringLocalizer _stringLocalizer;
+    private readonly IDialogService _dialogService;
 
-    public override string StackName => CommandStackNames.None;
+    public override string StackName => CommandStackNames.Project;
 
     public ResourceKey FolderResourceKey { get; set; }
 
+    private List<string> _pastedPaths = new();
+
     public PasteResourceCommand(
         IMessengerService messengerService,
-        IWorkspaceWrapper workspaceWrapper)
+        IWorkspaceWrapper workspaceWrapper,
+        IStringLocalizer stringLocalizer,
+        IDialogService dialogService)
     {
         _messengerService = messengerService;
         _workspaceWrapper = workspaceWrapper;
+        _stringLocalizer = stringLocalizer;
+        _dialogService = dialogService;
     }
 
     public override async Task<Result> ExecuteAsync()
+    {
+        var pasteResult = await PasteAsync();
+        if (pasteResult.IsFailure)
+        {
+            var titleString = _stringLocalizer.GetString("ResourceTree_Paste");
+            var messageString = _stringLocalizer.GetString("ResourceTree_PasteResourceFailed", FolderResourceKey);
+
+            await _dialogService.ShowAlertDialogAsync(titleString, messageString);
+        }
+
+        return pasteResult;
+    }
+
+    public override async Task<Result> UndoAsync()
+    {
+        var pasteResult = await UndoPasteAsync();
+        if (pasteResult.IsFailure)
+        {
+            var titleString = _stringLocalizer.GetString("ResourceTree_Paste");
+            var messageString = _stringLocalizer.GetString("ResourceTree_UndoPasteResourceFailed", FolderResourceKey);
+
+            await _dialogService.ShowAlertDialogAsync(titleString, messageString);
+        }
+
+        return pasteResult;
+    }
+
+    private async Task<Result> PasteAsync()
     {
         var resourceRegistry = _workspaceWrapper.WorkspaceService.ProjectService.ResourceRegistry;
 
@@ -48,7 +86,6 @@ public class PasteResourceCommand : CommandBase, IPasteResourceCommand
         var pasteFolder = await StorageFolder.GetFolderFromPathAsync(pasteFolderPath);
         var dataPackageView = Clipboard.GetContent();
 
-        bool modified = false;
         if (dataPackageView.Contains(StandardDataFormats.StorageItems))
         {
             var storageItems = await dataPackageView.GetStorageItemsAsync();
@@ -58,13 +95,11 @@ public class PasteResourceCommand : CommandBase, IPasteResourceCommand
                 {
                     if (storageItem is StorageFile storageFile)
                     {
-                        await storageFile.CopyAsync(pasteFolder);
-                        modified = true;
+                        await CopyStorageFileAsync(storageFile, pasteFolder);
                     }
                     else if (storageItem is StorageFolder storageFolder)
                     {
                         await CopyStorageFolderAsync(storageFolder, pasteFolder);
-                        modified = true;
                     }
                 }
             }
@@ -74,71 +109,81 @@ public class PasteResourceCommand : CommandBase, IPasteResourceCommand
             }
         }
 
-        if (modified)
+        if (_pastedPaths.Count == 0)
         {
-            resourceRegistry.SetFolderIsExpanded(FolderResourceKey, true);
-
-            var message = new RequestResourceTreeUpdate();
-            _messengerService.Send(message);
+            return Result.Fail("Failed to paste resources. No resources found in clipboard.");
         }
+
+        resourceRegistry.SetFolderIsExpanded(FolderResourceKey, true);
+
+        var message = new RequestResourceTreeUpdate();
+        _messengerService.Send(message);
 
         return Result.Ok();
     }
 
+    private async Task<Result> UndoPasteAsync()
+    {
+        foreach (var pastedPath in _pastedPaths)
+        {
+            try
+            {
+                if (File.Exists(pastedPath))
+                {
+                    File.Delete(pastedPath);
+                }
+                else if (Directory.Exists(pastedPath))
+                {
+                    Directory.Delete(pastedPath, true);
+                }
+            }
+            catch (Exception ex)
+            {
+                return Result.Fail($"Failed to undo paste resources. {ex}");
+            }
+        }
+        _pastedPaths.Clear();
+
+        var message = new RequestResourceTreeUpdate();
+        _messengerService.Send(message);
+
+        await Task.CompletedTask;
+
+        return Result.Ok();
+    }
+
+    private async Task CopyStorageFileAsync(StorageFile sourceFile, StorageFolder destinationFolder)
+    {
+        var newFile = await sourceFile.CopyAsync(destinationFolder, sourceFile.Name, NameCollisionOption.FailIfExists);
+        _pastedPaths.Add(newFile.Path);
+    }
+
     private async Task CopyStorageFolderAsync(StorageFolder sourceFolder, StorageFolder destinationFolder)
     {
-        try
+        if (sourceFolder == null || destinationFolder == null)
         {
-            if (sourceFolder == null || destinationFolder == null)
-            {
-                throw new ArgumentNullException("Source or destination folder is null.");
-            }
-
-            var destinationSubFolder = await destinationFolder.CreateFolderAsync(sourceFolder.Name, CreationCollisionOption.OpenIfExists);
-
-            var files = await sourceFolder.GetFilesAsync();
-            foreach (var file in files)
-            {
-                try
-                {
-                    await file.CopyAsync(destinationSubFolder, file.Name, NameCollisionOption.ReplaceExisting);
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine($"Error copying file {file.Name}: {ex.Message}");
-                }
-            }
-
-            var subFolders = await sourceFolder.GetFoldersAsync();
-            foreach (var subFolder in subFolders)
-            {
-                try
-                {
-                    await CopyStorageFolderAsync(subFolder, destinationSubFolder);
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine($"Error copying subfolder {subFolder.Name}: {ex.Message}");
-                }
-            }
+            throw new ArgumentNullException("Source or destination folder is null.");
         }
-        catch (UnauthorizedAccessException ex)
+
+        var destinationSubFolder = await destinationFolder.CreateFolderAsync(sourceFolder.Name, CreationCollisionOption.FailIfExists);
+        _pastedPaths.Add(destinationSubFolder.Path);
+
+        var files = await sourceFolder.GetFilesAsync();
+        foreach (var file in files)
         {
-            Console.WriteLine($"Access denied: {ex.Message}");
+            await CopyStorageFileAsync(file, destinationSubFolder);
         }
-        catch (FileNotFoundException ex)
+
+        var subFolders = await sourceFolder.GetFoldersAsync();
+        foreach (var subFolder in subFolders)
         {
-            Console.WriteLine($"File not found: {ex.Message}");
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"An error occurred: {ex.Message}");
+            await CopyStorageFolderAsync(subFolder, destinationSubFolder);
         }
     }
 
     public static void PasteResource(ResourceKey folderResourceKey)
     {
         var commandService = ServiceLocator.ServiceProvider.GetRequiredService<ICommandService>();
-        commandService.Execute<ICopyResourceCommand>(command => command.ResourceKey = folderResourceKey);
+        commandService.Execute<IPasteResourceCommand>(command => command.FolderResourceKey = folderResourceKey);
     }
 }
