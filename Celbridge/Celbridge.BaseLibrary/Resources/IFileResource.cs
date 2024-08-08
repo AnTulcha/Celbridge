03cee151fe0d8475165f8aa30bea003dd538cb37
Chris Gregan <chrisgregan@gmail.com>
2024-08-08 18:09:23 +0100

    Initial support for file-icons font

--- Celbridge/Celbridge.BaseLibrary/Resources/IFileResource.cs
+++ Celbridge/Celbridge.BaseLibrary/Resources/IFileResource.cs
@@ -4,4 +4,8 @@
 /// A file resource in the project folder.
 /// </summary>
 public interface IFileResource : IResource
-{}
+{
+    string IconGlyph { get; }
+
+    string IconColor { get; }
+}
--- Celbridge/CoreExtensions/Celbridge.Resources/Models/FileResource.cs
+++ Celbridge/CoreExtensions/Celbridge.Resources/Models/FileResource.cs
@@ -2,6 +2,10 @@
 
 public class FileResource : Resource, IFileResource
 {
+    public string IconGlyph { get; set; } = "u";
+
+    public string IconColor { get; set; } = "LightBlue";
+
     public FileResource(string name, IFolderResource parentFolder) 
         : base(name, parentFolder)
     { }
