d0e51017554444b4be32f1fa28c10fdaa1a2aade
Chris Gregan <chrisgregan@gmail.com>
2024-10-03 18:50:37 +0100

    Use localized strings for New Project and Open Project buttons

--- Celbridge/CoreServices/Celbridge.UserInterface/Views/HomePage.xaml.cs
+++ Celbridge/CoreServices/Celbridge.UserInterface/Views/HomePage.xaml.cs
@@ -2,12 +2,20 @@ namespace Celbridge.UserInterface.Views;
 
 public sealed partial class HomePage : Page
 {
+    private IStringLocalizer _stringLocalizer;
+
+    public LocalizedString NewProjectString => _stringLocalizer.GetString("MainPage_NewProject");
+    public LocalizedString OpenProjectString => _stringLocalizer.GetString("MainPage_OpenProject");
+
     public HomePageViewModel ViewModel { get; private set; }
 
     public HomePage()
     {
         this.InitializeComponent();
 
-        ViewModel = ServiceLocator.ServiceProvider.GetRequiredService<HomePageViewModel>();
+        var serviceProvider = ServiceLocator.ServiceProvider;
+        ViewModel = serviceProvider.GetRequiredService<HomePageViewModel>();
+
+        _stringLocalizer = serviceProvider.GetRequiredService<IStringLocalizer>();
     }
 }
