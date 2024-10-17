511f74e120587ef4ebc33154e02e3ea25512e36a
Chris Gregan <chrisgregan@gmail.com>
2024-10-17 07:12:57 +0100

    Add tooltip to web inspector open button

--- Celbridge/Modules/Workspace/Celbridge.Inspector/Views/WebInspector.cs
+++ Celbridge/Modules/Workspace/Celbridge.Inspector/Views/WebInspector.cs
@@ -9,6 +9,7 @@ public partial class WebInspector : UserControl, IInspector
 
     public WebInspectorViewModel ViewModel => (DataContext as WebInspectorViewModel)!;
     private LocalizedString StartURLString => _stringLocalizer.GetString("WebInspector_StartURL");
+    private LocalizedString OpenURLTooltipString => _stringLocalizer.GetString("WebInspector_OpenURLTooltip");
 
     public ResourceKey Resource 
     {
@@ -30,31 +31,41 @@ public partial class WebInspector : UserControl, IInspector
         DataContext = viewModel;
 
         this.DataContext<WebInspectorViewModel>((inspector, vm) => inspector
-            .Content(
-                new Grid()
-                    .ColumnDefinitions("*, auto") 
-                    .HorizontalAlignment(HorizontalAlignment.Stretch)
-                    .Children(
-                        new TextBox()
-                            .Grid(column: 0)
-                            // This appears to be the right way to bind to a localized string.
-                            // Just doing .Header(StartURLString) throws a cryptic XAML exception.
-                            // https://platform.uno/docs/articles/external/uno.extensions/doc/Learn/Markup/Binding101.html
-                            .Header(() => inspector.StartURLString)
+            .Content
+            (
+                new StackPanel()
+                    .Orientation(Orientation.Vertical)
+                    .Children
+                    (
+                        new Grid()
+                            .ColumnDefinitions("*, auto")
                             .HorizontalAlignment(HorizontalAlignment.Stretch)
-                            .Text(x => x.Binding(() => vm.Url)
-                                .Mode(BindingMode.TwoWay)),
-                        new Button()
-                            .Grid(column: 1)
-                            .Margin(2, 0, 2, 0)
-                            .VerticalAlignment(VerticalAlignment.Bottom)
-                            .Command(ViewModel.RefreshCommand)
-                            .Content(
-                                new SymbolIcon()
-                                .Symbol(Symbol.Forward)
+                            .Children
+                            (
+                                new TextBox()
+                                    .Grid(column: 0)
+                                    // This appears to be the right way to bind to a localized string.
+                                    // Just doing .Header(StartURLString) throws a cryptic XAML exception.
+                                    // https://platform.uno/docs/articles/external/uno.extensions/doc/Learn/Markup/Binding101.html
+                                    .Header(() => inspector.StartURLString)
+                                    .HorizontalAlignment(HorizontalAlignment.Stretch)
+                                    .Text(x => x.Binding(() => vm.Url)
+                                        .Mode(BindingMode.TwoWay)),
+                                new Button()
+                                    .Grid(column: 1)
+                                    .Margin(2, 0, 2, 0)
+                                    .VerticalAlignment(VerticalAlignment.Bottom)
+                                    .Command(ViewModel.RefreshCommand)
+                                    // The binding technique above doesn't work for tooltips though.
+                                    // In this case you seem to have to reference the string directly.
+                                    .ToolTipService(null, null, inspector.OpenURLTooltipString)
+                                    .Content
+                                    (
+                                        new SymbolIcon()
+                                            .Symbol(Symbol.Forward)
+                                    )
                             )
                     )
-            )   
-        );
+            ));
     }
 }
