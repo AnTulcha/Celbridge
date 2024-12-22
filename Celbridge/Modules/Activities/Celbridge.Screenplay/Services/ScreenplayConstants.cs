fb59a319f6598c2f048dfa70e56c2159205db8a4
Chris Gregan <chrisgregan@gmail.com>
2024-12-22 09:44:08 +0000

    Add ScreenplayConstants static class

---
+++ Celbridge/Modules/Activities/Celbridge.Screenplay/Services/ScreenplayConstants.cs
@@ -0,0 +1,14 @@
+namespace Celbridge.Screenplay.Services;
+
+public static class ScreenplayConstants
+{
+    public const string ScreenplayActivityName = "Screenplay";
+    public const string SceneComponentType = "Scene";
+    public const string LineComponentType = "Line";
+
+    public const string SceneComponentProperty_SceneTitle = "/sceneTitle";
+    public const string SceneComponentProperty_SceneDescription = "/sceneDescription";
+
+    public const string LineComponentProperty_Character = "/character";
+    public const string LineComponentProperty_SourceText = "/sourceText";
+}
