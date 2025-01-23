52352175151885946074f2cf8545fc0ac0027ede
Chris Gregan <chrisgregan@gmail.com>
2025-01-23 14:42:29 +0000

    Move CreateComponentConfig() to ComponentConfigRegistry

--- Celbridge/Modules/Workspace/Celbridge.Entities/Services/ComponentConfigRegistry.cs
+++ Celbridge/Modules/Workspace/Celbridge.Entities/Services/ComponentConfigRegistry.cs
@@ -1,17 +1,26 @@
 using Celbridge.Entities.Models;
+using Celbridge.Forms;
+using Json.More;
+using Json.Pointer;
+using Json.Schema;
+using System.Text.Json;
 
 namespace Celbridge.Entities.Services;
 
 public class ComponentConfigRegistry
 {
     private readonly IServiceProvider _serviceProvider;
+    private readonly IFormService _formService;
 
     private readonly Dictionary<string, ComponentConfig> _componentConfigs = new();
     public IReadOnlyDictionary<string, ComponentConfig> ComponentConfigs => _componentConfigs;
 
-    public ComponentConfigRegistry(IServiceProvider serviceProvider)
+    public ComponentConfigRegistry(
+        IServiceProvider serviceProvider,
+        IFormService formService)
     {
         _serviceProvider = serviceProvider;
+        _formService = formService;
     }
 
     public Result Initialize()
@@ -42,7 +51,7 @@ public class ComponentConfigRegistry
                 var configJson = loadConfigResult.Value;
 
                 // Create the component config
-                var createResult = ComponentConfig.CreateConfig(editorType, configJson);
+                var createResult = CreateComponentConfig(editorType, configJson);
                 if (!createResult.IsSuccess)
                 {
                     return Result.Fail($"Failed to create component config from JSON for component editor: '{editorKey}'")
@@ -85,6 +94,7 @@ public class ComponentConfigRegistry
                 .WithException(ex);
         }
     }
+
 
     public Result<ComponentConfig> GetComponentConfig(string componentType)
     {
@@ -158,5 +168,172 @@ public class ComponentConfigRegistry
         }
 
         return Result<string>.Ok(json);
+    }
+
+    private Result<ComponentConfig> CreateComponentConfig(Type componentEditorType, string schemaJson)
+    {
+        try
+        {
+            using var document = JsonDocument.Parse(schemaJson);
+            var root = document.RootElement;
+
+            if (root.ValueKind != JsonValueKind.Object)
+            {
+                return Result<ComponentConfig>.Fail("Failed to parse schema JSON as an object");
+            }
+
+            // Get component type and version
+
+            var componentTypePointer = JsonPointer.Parse(ComponentConfig.ComponentTypeConstKey);
+            var componentTypeElement = componentTypePointer.Evaluate(root);
+
+            if (componentTypeElement is null ||
+                componentTypeElement.Value.ValueKind != JsonValueKind.String)
+            {
+                return Result<ComponentConfig>.Fail("Component type element is not valid");
+            }
+
+            var typeAndVersion = componentTypeElement.Value.GetString();
+            if (string.IsNullOrEmpty(typeAndVersion))
+            {
+                return Result<ComponentConfig>.Fail("Component type is empty");
+            }
+
+            var parseResult = EntityUtils.ParseComponentTypeAndVersion(typeAndVersion);
+            if (parseResult.IsFailure)
+            {
+                return Result<ComponentConfig>.Fail($"Failed to parse component type and version: {typeAndVersion}")
+                    .WithErrors(parseResult);
+            }
+            var (componentType, componentVersion) = parseResult.Value;
+
+            // Get the component attributes
+
+            var componentTags = new HashSet<string>();
+            var componentAttributes = new Dictionary<string, string>();
+
+            if (root.TryGetProperty(ComponentConfig.AttributesKey, out JsonElement attributesElement))
+            {
+                foreach (var attribute in attributesElement.EnumerateObject())
+                {
+                    if (attribute.Name == "tags")
+                    {
+                        // Tags are treated specially to support fast querying
+
+                        foreach (var tag in attribute.Value.EnumerateArray())
+                        {
+                            if (tag.ValueKind != JsonValueKind.String)
+                            {
+                                return Result<ComponentConfig>.Fail("Tag value is not a string");
+                            }
+
+                            componentTags.Add(tag.ToString());
+                        }
+                    }
+                    else
+                    {
+                        componentAttributes[attribute.Name] = attribute.Value.ToString();
+                    }
+                }
+            }
+
+            // Add the component namespace as a tag for convenience
+            var dotIndex = componentType.LastIndexOf('.');
+            if (dotIndex > 0)
+            {
+                var componentNamespace = componentType.Substring(0, dotIndex);
+                if (!string.IsNullOrEmpty(componentNamespace))
+                {
+                    componentTags.Add(componentNamespace);
+                }
+            }
+
+            // Get the component properties
+
+            var componentProperties = new List<ComponentPropertyInfo>();
+            if (root.TryGetProperty(ComponentConfig.PropertiesKey, out JsonElement propertiesElement))
+            {
+                foreach (var propertyElement in propertiesElement.EnumerateObject())
+                {
+                    var propertyName = propertyElement.Name;
+                    if (propertyName.StartsWith('_'))
+                    {
+                        // Ignore internal-only properties
+                        continue;
+                    }
+
+                    var propertyType = propertyElement.Value.GetProperty(ComponentConfig.TypeKey).ToString();
+                    var propertyAttributes = new Dictionary<string, string>();
+                    if (propertyElement.Value.TryGetProperty(ComponentConfig.AttributesKey, out JsonElement propertyAttributesElement))
+                    {
+                        foreach (var attribute in propertyAttributesElement.EnumerateObject())
+                        {
+                            propertyAttributes[attribute.Name] = attribute.Value.ToString();
+                        }
+                    }
+
+                    var propertyInfo = new ComponentPropertyInfo(propertyName, propertyType, propertyAttributes);
+                    componentProperties.Add(propertyInfo);
+                }
+            }
+
+            // Register the form (if one is specified)
+
+            if (root.TryGetProperty(ComponentConfig.FormKey, out JsonElement formElement))
+            {
+                if (formElement.ValueKind != JsonValueKind.Array)
+                {
+                    return Result<ComponentConfig>.Fail("Form json does not contain an array of form elements");
+                }
+                var formJson = formElement.ToJsonString();
+
+                var formResult = _formService.RegisterForm(componentType, formJson, FormScope.Workspace);
+                if (formResult.IsFailure)
+                {
+                    return Result<ComponentConfig>.Fail($"Failed to register form for component type: {componentType}")
+                        .WithErrors(formResult);
+                }
+            }
+
+            // Build the component schema
+
+            var componentSchema = new ComponentSchema(componentType, componentVersion, componentTags, componentAttributes, componentProperties);
+
+            // Construct the prototype element
+
+            var prototypeNode = root.GetProperty(ComponentConfig.PrototypeKey).AsNode();
+            if (prototypeNode is null)
+            {
+                return Result<ComponentConfig>.Fail("Prototype node not found");
+            }
+            prototypeNode["_componentType"] = typeAndVersion; // Prototype type and version match the schema
+
+            var prototype = JsonSerializer.Deserialize<JsonElement>(prototypeNode.ToJsonString());
+
+            // Create the JsonSchema object
+
+            var jsonSchema = JsonSchema.FromText(schemaJson);
+            if (jsonSchema is null)
+            {
+                return Result<ComponentConfig>.Fail($"Failed to parse JSON schema for component type: '{componentType}'");
+            }
+
+            // Validate the prototype
+
+            var evaluateResult = jsonSchema.Evaluate(prototype);
+            if (!evaluateResult.IsValid)
+            {
+                return Result<ComponentConfig>.Fail($"Prototype failed schema validation: '{componentType}'");
+            }
+
+            var config = new ComponentConfig(componentType, componentVersion, componentSchema, prototype, jsonSchema, componentEditorType);
+
+            return Result<ComponentConfig>.Ok(config);
+        }
+        catch (Exception ex)
+        {
+            return Result<ComponentConfig>.Fail("An exception occurred when creating component config.")
+                .WithException(ex);
+        }
     }
 }
