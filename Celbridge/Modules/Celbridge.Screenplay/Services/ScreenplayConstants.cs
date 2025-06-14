cf5c320ef2ab26ddfb9769aefd7df4335c2a1ecd
Chris Gregan <chrisgregan@gmail.com>
2025-06-14 15:26:02 +0100

    Add comment for ModifiedScenesKey

--- Celbridge/Modules/Celbridge.Screenplay/Services/ScreenplayConstants.cs
+++ Celbridge/Modules/Celbridge.Screenplay/Services/ScreenplayConstants.cs
@@ -2,5 +2,8 @@ namespace Celbridge.Screenplay.Services;
 
 public static class ScreenplayConstants
 {
+    /// <summary>
+    /// Workspace settings key for modified scenes list.
+    /// </summary>
     public const string ModifiedScenesKey = "ModifiedScenes";
 }
